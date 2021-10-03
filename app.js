2b406b764ea5c48b4854d3517449c42690c24016
mohamed elkony <konykony22@gmail.com>
2021-10-03 11:43:19 +0200

    0.0.2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { RSA_PKCS1_OAEP_PADDING } = require("constants");
 const exp = require("constants");
 const express=require("express");
 const path=require('path');
@@ -5,6 +6,7 @@ const app =express();
 
 app.use('/',express.static(path.join(__dirname,'public','frontend')));
 app.use('/',express.json());
+
 const users=[];
 app.get('/',function(req,res){
 res.sendFile(path.join(__dirname,'public','frontend','index.html'));
@@ -13,7 +15,15 @@ app.post('/signup',(req,res)=>{
     users.push(req.body);
     res.end();
 })
+app.get('/login',(req,res)=>{
+res.sendFile(path.join(__dirname,'public','frontend','log in.html'));
+})
+app.get('/signup',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','frontend','sign up.html'));
+})
+    
 app.get('/users',(req,res)=>{
     res.send(JSON.stringify(users));
 })
+
 app.listen(3000,function(){console.log(`server started`);});
