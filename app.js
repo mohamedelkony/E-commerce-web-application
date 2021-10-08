b3712c0a33f3fe3836687818b3490e318e3d3187
mohamed elkony <konykony22@gmail.com>
2021-10-08 09:53:35 +0200

    0.0.2

--- app.js
+++ app.js
@@ -7,21 +7,42 @@ app.use('/', express.static(path.join(__dirname, 'public', 'frontend')));
 app.use('/', express.json());
 app.use('/', express.urlencoded({ extend: true }));
 
-const users = [];
+const users = [
+    {
+        username: 'mohamed_elkony',
+        password: '123456',
+        gender: 'male',
+        email: 'konykony22@gmail.com',
+        birth: new Date().toUTCString(),
+    }
+];
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'frontend', 'index.html'));
 })
-app.post('/signup', async (req, res) => {
+app.post('/users', async (req, res) => {
     try {
         const tst = await valdiateSignUP(req.body);
         users.push(req.body);
         res.redirect('/users');
     } catch (error) {
-        console.log("XX"+error.toString());
-        res.status(410);
+        console.log("not valid data :" + error.toString());
+        res.status(422);
         res.send(error.toString());
     }
     res.end();
+})
+app.post('/login', (req, res) => {
+    try {
+        const user = users.find(e => e.email === req.body.email);
+        if (user != undefined && user.password === req.body.password) {
+            res.redirect(`/profile/${user.username}`);
+        }
+        else
+            res.send('wrong email or password');
+    }
+    catch (err) {
+        res.send("ERror" + res.req.toString());
+    }
 })
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'frontend', 'log in.html'));
@@ -29,20 +50,31 @@ app.get('/login', (req, res) => {
 app.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'frontend', 'sign up.html'));
 })
-
-app.get('/users', (req, res) => {
-    res.send(JSON.stringify(users));
+app.get('/profile/:username', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'frontend', 'profile.html'));
 })
 
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+app.get('/users/:username', (req, res) => {
+    const user = users.find(e => { return e.username === req.params.username; });
+    if (user == undefined) {
+        res.status(404);
+        res.end();
+    }
+    res.send(user);
+})
 app.listen(3000, function () { console.log(`server started`); });
 
 async function valdiateSignUP(data) {
     const schema = Joi.object({
-        name: Joi.string().token().max(25),
+        username: Joi.string().token().max(25),
         password: Joi.string().min(6).max(100),
         gender: Joi.number(),
         email: Joi.string().email(),
-        birth: Joi.date()
+        birth: Joi.date(),
+        repassword: Joi.ref('password')
     });
     return schema.validateAsync(data);
 }
