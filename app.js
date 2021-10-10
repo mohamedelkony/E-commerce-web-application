efb0d6b3f7ad7f2667facab710b78b08388b1d8f
mohamed elkony <konykony22@gmail.com>
2021-10-10 14:04:33 +0200

    0.0.3

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const path = require('path');
 const Joi = require('joi');
+const sessions = require('express-session');
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public', 'frontend')));
 app.use('/', express.json());
-app.use('/', express.urlencoded({ extend: true }));
+app.use('/', express.urlencoded({ extended: true }));
+app.use(sessions({
+    secret: '32165asdassdahghgfbdfs875',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }
+}));
 
 const users = [
     {
@@ -16,44 +23,21 @@ const users = [
         birth: new Date().toUTCString(),
     }
 ];
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'frontend', 'index.html'));
-})
 app.post('/users', async (req, res) => {
     try {
         const tst = await valdiateSignUP(req.body);
-        users.push(req.body);
-        res.redirect('/users');
+        const emailused = users.find(e => e.email === req.body.email);
+        if (emailused != undefined)
+            res.status(301).send('email already used!');
+        else {
+            users.push(req.body);
+            res.redirect(`/profile/${req.body.username}`);
+        }
     } catch (error) {
         console.log("not valid data :" + error.toString());
-        res.status(422);
-        res.send(error.toString());
-    }
-    res.end();
-})
-app.post('/login', (req, res) => {
-    try {
-        const user = users.find(e => e.email === req.body.email);
-        if (user != undefined && user.password === req.body.password) {
-            res.redirect(`/profile/${user.username}`);
-        }
-        else
-            res.send('wrong email or password');
-    }
-    catch (err) {
-        res.send("ERror" + res.req.toString());
+        res.status(422).send(error.toString());
     }
 })
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'frontend', 'log in.html'));
-})
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'frontend', 'sign up.html'));
-})
-app.get('/profile/:username', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'frontend', 'profile.html'));
-})
-
 app.get('/users', (req, res) => {
     res.send(users);
 })
@@ -65,15 +49,55 @@ app.get('/users/:username', (req, res) => {
     }
     res.send(user);
 })
+
+app.get('/',(req,res)=>
+{
+    res.status(500).send();
+})
+app.post('/login', (req, res) => {
+    try {
+        const user = users.find(e => e.email === req.body.email);
+        if (user != undefined && user.password === req.body.password) {
+            req.session.username = user.username;
+            res.redirect(`/profile/${user.username}`);
+        }
+        else
+            res.send('wrong email or password');
+    }
+    catch (err) {
+        res.send("ERror" + res.req.toString());
+    }
+})
+app.get('/login', (req, res) => {
+    if (req.session.username)
+        res.redirect(`/profile/${req.session.username}`);
+    else
+        res.sendFile(path.join(__dirname, 'public', 'frontend', 'log in.html'));
+})
+app.get('/logout', (req, res) => {
+    if (req.session.username)
+        req.session.destroy((err) => {
+            if (err)
+                res.status(500).send(`error can't logout`);
+            else
+                res.redirect('/');
+        })
+})
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'frontend', 'sign up.html'));
+})
+app.get('/profile/:username', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'frontend', 'profile.html'));
+})
 app.listen(3000, function () { console.log(`server started`); });
 
 async function valdiateSignUP(data) {
     const schema = Joi.object({
-        username: Joi.string().token().max(25),
-        password: Joi.string().min(6).max(100),
-        gender: Joi.number(),
-        email: Joi.string().email(),
-        birth: Joi.date(),
+        username: Joi.string().token().max(25).required(),
+        password: Joi.string().min(3).max(300).required(),
+        gender: Joi.string().valid('male', 'female').required(),
+        email: Joi.string().email().required(),
+        birth: Joi.date().required(),
         repassword: Joi.ref('password')
     });
     return schema.validateAsync(data);
