3977ea73cfb58c4809b2d006407da306cd4b12df
mohamed_elkony <konykony22@gmail.com>
2022-02-17 22:57:57 +0200

    remove build js folder from repo

--- build/util/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function asyncHandler(code) {
-    return function (req, res, next) {
-        return Promise.resolve(code(req, res, next)).catch(next);
-    };
-}
-exports.default = asyncHandler;
