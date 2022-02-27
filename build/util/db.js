498e68610e077fa2400f88aa94004a2454336b08
mohamed_elkony <konykony22@gmail.com>
2022-02-28 01:19:12 +0200

    heroku deply audit

--- build/util/db.js
+++ build/util/db.js
@@ -2,8 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 console.log('XXXXXXXXX');
-const pool = new pg_1.Pool();
-console.log(pg_1.Pool.length);
+let pool = null;
+var dbhost = null;
+if (process.env.NODE_ENV == 'Production') {
+    pool = new pg_1.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+    dbhost = process.env.DATABASE_URL;
+}
+else {
+    pool = new pg_1.Pool();
+    dbhost = 'localhost';
+}
+console.log('connected to database @' + dbhost);
 exports.default = {
     query: (text, params) => pool.query(text, params),
     pool,
