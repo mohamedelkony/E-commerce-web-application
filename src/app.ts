f2b65898216cdeac41994eef0f6d0e5f71230917
mohamedelkony <konykony22@gmail.com>
2024-12-26 23:35:55 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,6 @@ const dotenv = require('dotenv');
 const compression = require('compression')
 const pgSession = require('connect-pg-simple')(session);
 import db from './util/db'
-
-
 //app.use(helmet())
 // compress all responses
 app.use(compression())
