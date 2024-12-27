5d4cc802668ff7c8ff09cd8221e09e5360d8c747
mohamedelkony <konykony22@gmail.com>
2024-12-28 01:09:24 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,8 @@ app.use(compression())
 dotenv.config();
 console.log('server is booting ...')
 
+
+
 app.set('views', path.join(__dirname, '..', 'views'))
 app.set('view engine', 'ejs')
 app.use('/', express.static(path.join(__dirname, '..', 'public')))
